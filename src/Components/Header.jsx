fb951d047174587fb517776b7337430cae46763e
Mahmoud Elsherbiny <mami55209@gmail.com>
2024-07-02 14:38:09 +0300

    dynamic banners

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     const cartItems = useSelector(state=>state.cart.cartItems);
     const baseURL = `${import.meta.env.VITE_REACT_APP_API_URL}`; 
     const logo = useSelector(state=>state.banners.logo); 
-    const img_url = logo && logo?.img_url;
+    const img_url = logo?.img_url;
   return (
     <header className='header bg-white'>
     <Navbar expand="lg" className='p-0' dir='rtl'>
